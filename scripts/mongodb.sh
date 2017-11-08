c63d894ad5ffdf475b7154e27e6778fc816d503f
Francis <francis.menkes@gmail.com>
2017-11-08 11:23:50 -0300

    Update mongo script

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -4,6 +4,6 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
 apt-get install -y mongodb-org
-cp /vagrant/files/mongodb.service /etc/systemd/system/mongodb.service
+cp /etc/vagrant/mongodb.service /etc/systemd/system/mongodb.service
 systemctl start mongodb
 export LC_ALL=C
