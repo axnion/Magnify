220d7336d9b2a0d2b61d7efd756a01541e853a7a
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-09 17:28:20 +0100

    Add example API route

--- backend/routes.js
+++ backend/routes.js
@@ -1,14 +1,12 @@
-const Router = require('express').Router;
+const Router = require("express").Router;
 const router = new Router();
 
-const user = require('./model/user/router');
-const product = require('./model/product/router');
+const example = require("./model/example/router");
 
-router.route('/').get((req, res) => {
-  res.json({ message: 'Welcome to backend API!' });
+router.route("/").get((req, res) => {
+  res.json({ message: "Welcome to backend API!" });
 });
 
-router.use('/user', user);
-router.use('/product', product);
+router.use("/example", example);
 
 module.exports = router;
