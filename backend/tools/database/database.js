ffebf301ea846e74242813d73d11c6bc35a25463
Axel Nilsson <an222yp@student.lnu.se>
2017-11-10 20:35:50 +0100

    Moved admintools to backend/tools

--- backend/tools/database/database.js
+++ backend/tools/database/database.js
@@ -1,6 +1,8 @@
+const config = require('../../config.js')
 const mongoose = require('mongoose')
 const Company = require('./schemas/Company')
 
+mongoose.connect(config.mongo.url)
 
 exports.addCompany = function(name) {
   const company = Company({
