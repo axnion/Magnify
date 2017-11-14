9c41e56d3a60652512e52dbceddae20e9f896170
Leif Karlsson <leif.karlsson@stavos.ax>
2017-11-14 14:52:01 +0200

    adds script to setup production deployment

---
+++ scripts/deploy-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PROD_URI="magnify.today"
+PROD_PORT=2212
+PROD_USER="magnify"
+
+# Generate SSH keys used by git
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
+
+# Copy private key to production server
+# Password: <secret>
+ssh-copy-id -p $PROD_PORT -o StrictHostKeyChecking=no $PROD_USER@$PROD_URI
