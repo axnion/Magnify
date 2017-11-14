ff1eb545df98f124929bef69c833befe948753f3
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-14 03:19:20 +0100

    Add passing test for GET of specific company

--- backend/test/company_test.js
+++ backend/test/company_test.js
@@ -25,5 +25,68 @@ describe("Company", () => {
           done();
         });
     });
+
+    it("Should GET a specific company", done => {
+      const company = new Company({
+        name: "Awesome corp"
+      });
+
+      chai
+        .request(server)
+        .post("/company")
+        .send(company)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+          res.body.should.have.property("name");
+          res.body.name.should.equal("Awesome corp");
+          done();
+        });
+    });
+  });
+
+  describe("POST company", () => {
+    it("Should save a newly created company", done => {
+      const company = new Company({
+        name: "Awesome corp"
+      });
+
+      chai
+        .request(server)
+        .post("/company")
+        .send(company)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+
+    it("Should not create duplicate companies", done => {
+      const companyA = new Company({
+        name: "Awesome corp"
+      });
+
+      const companyB = new Company({
+        name: "Awesome corp"
+      });
+
+      chai
+        .request(server)
+        .post("/company")
+        .send(companyA)
+        .send(companyB)
+        .end((err, res) => {
+          res.should.have.status(201);
+          chai
+            .request(server)
+            .post("/company")
+            .send(companyB)
+            .end((err, res) => {
+              res.should.have.status(500);
+              done();
+            });
+        });
+    });
   });
 });
