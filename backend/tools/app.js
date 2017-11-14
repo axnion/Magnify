371b3b2208f727eed335c97348d138878984304f
Axel Nilsson <an222yp@student.lnu.se>
2017-11-15 00:16:10 +0100

    Backend: Modifications to admin tool for future expation and ease of use

--- backend/tools/app.js
+++ backend/tools/app.js
@@ -7,40 +7,92 @@ const accountFacade = require("../model/account/facade");
 mongoose.Promise = require("bluebird");
 mongoose.connect(config.mongo.url, { useMongoClient: true });
 
+mongoose.connection.close()
+
+/**
+* Create new account.
+*/
+const createAccount = function(username, password, company, admin) {
+  console.log("1")
+  companyFacade.findOne({ name: company })
+  .then(function(results) {
+  console.log("2")
+    if(!results && admin) {
+      console.log("3")
+      return companyFacade.create({ name: company })
+    } else {
+      console.log("4")
+      return new Promise(function(resolve, reject) {
+        if(results) {
+          console.log("5")
+          resolve(results)
+        } else {
+          console.log("5")
+          reject("No company matchin \"" + company + "\" was found to add this user to")
+        }
+      })
+    }
+  })
+  .then(function(company) {
+    return accountFacade.create({
+      username: commander.username,
+      password: commander.password,
+      admin: commander.admin,
+      company:company.id
+    })
+  })
+  .then(function(account) {
+    console.log("Created User-----------------------------------------------")
+    console.log(account + "\n")
+    mongoose.connection.close()
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+}
+
+// Specifying version
 commander
   .version("1.0")
-  .option("-c, --company [name]", "Specify company name")
-  .option("-u, --username [username]", "Specify username")
-  .option("-p, --password [password]", "Specify password")
-  .parse(process.argv);
 
-if (commander.company) {
-  if (!commander.username) {
-    console.log("Please specify a username");
-  }
-  if (!commander.password) {
-    console.log("Please specify a password");
-  }
-  if (!commander.company) {
-    console.log("Please specify a company");
-  }
+// Account subcommand
+commander
+    .command('account')
+    .description('Actions dealing with accounts')
+    .option('-C, --create', 'Creates an account')
+    .option("-u, --username [username]", "Specify username")
+    .option("-p, --password [password]", "Specify password")
+    .option("-a, --admin", "Specifies the new user as a company admin")
+    .option("-c, --company [name]", "Specify company name")
+    .action(function(flags) {
+      if (flags.create) {
+        if (!flags.username) {
+          console.log("Please specify username")
+        }
+        if (!flags.password) {
+          console.log("Please specify a password")
+        }
+        if (!flags.company) {
+          console.log("Please specify a company")
+        }
+        if (!flags.admin) {
+          flags.admin = false
+        }
 
-  companyFacade
-    .addCompany({ name: commander.company })
-    .then(function(results) {
-      console.log("Added company");
+        createAccount(flags.username, flags.password, flags.company, flags.admin)
 
-      return accountFacade.create({
-        username: commander.username,
-        password: commander.password,
-        admin: true,
-        company: results.id
-      });
+      } else {
+        console.log("No valid action found")
+      }
     })
-    .then(function() {
-      console.log("done");
+
+// Company Subcommand
+commander
+    .command('company')
+    .option('-A, -add', 'Adds a company')
+    .description('Actions dealing with companies')
+    .action(function() {
+      console.log("COMPANY!")
     })
-    .catch(function(err) {
-      console.log(err);
-    });
-}
+
+commander.parse(process.argv);
