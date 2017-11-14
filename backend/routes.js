9bddd932ad73431191b9f7eaa80a854a2c6c86df
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-14 02:30:41 +0100

    Add passing test for GET of /company

--- backend/routes.js
+++ backend/routes.js
@@ -3,6 +3,7 @@ const router = new Router();
 
 const example = require("./model/example/router");
 const account = require("./model/account/router");
+const company = require("./model/company/router");
 
 router.route("/").get((req, res) => {
   res.json({ message: "Welcome to backend API!" });
@@ -10,5 +11,6 @@ router.route("/").get((req, res) => {
 
 router.use("/account", account);
 router.use("/example", example);
+router.use("/company", company);
 
 module.exports = router;
