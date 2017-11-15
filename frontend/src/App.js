29b3c3b0828494ff12be987821ce0add60b77326
Francis <francis.menkes@gmail.com>
2017-11-15 10:44:39 -0300

    Frontend: add styled-components, convert sideBar to styled component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Link, Route} from 'react-router-dom'
+import { Link, Route} from 'react-router-dom';
+import styled from 'styled-components';
 
 import './App.css'
 import topBanner from './topbanner.jpg';
 import leftside from './leftside.jpg';
-import AddRepFormContainer from './containers/AddRepFormContainer.js'
-import Login from './containers/LoginContainer.js'
+import AddRepFormContainer from './containers/AddRepFormContainer.js';
+import Login from './containers/LoginContainer.js';
 
 //Put this here to add background image, all other css is in css file
-const sideBarStyle = {
-  width: "178px",
-  height: "671px",
-  backgroundImage: "url(" + leftside + ")",
-  borderRight: "1px solid black",
-  float: "left",
-  textAlign: "center",
-}
+const SideBar = styled('div')`
+  width: 178px;
+  height: 671px;
+  background-image: url(${leftside});
+  border-right: 1px solid black;
+  float: left;
+  text-align: center;
+`;
 
 export default () => {
   return (
@@ -23,10 +24,10 @@ export default () => {
       <header className="App-header">
         <Link to="/"><img src={topBanner} className="App-top-banner" alt="Top banner" /></Link>
       </header>
-      <div style={sideBarStyle}>
+      <SideBar>
         <Link to="/login"><button className="sidebar-btn">Log in</button></Link><br/>
         <Link to="/addRep"><button className="sidebar-btn">Add representative</button></Link>
-      </div>
+      </SideBar>
       <div className="App-content">
         <Route path="/addRep" component={AddRepFormContainer} />
         <Route path="/login" component={Login} />
