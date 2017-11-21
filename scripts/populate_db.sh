fb63a7647b2125fa985735f72d88101aae1c64e5
Axel Nilsson <an222yp@student.lnu.se>
2017-11-21 21:42:34 +0100

    Backend: populate_db.sh is now live adding a company and one user of each kind

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -3,9 +3,23 @@
 # Change to tool directroy
 cd ../backend/tools/
 
-# Drop accounts collection
-node app
+node app.js account --drop # Drop accounts collection
+sleep 5s
+node app.js company --drop # Drop companies collection
+sleep 5s
+# TODO Drop categories collection
 
-# Drop companies collection
+# Create companies
+node app.js company -C -n AwesomeCorp
+sleep 5s
 
-# TODO: Drop categories collection
+node app.js account -C -u mrconsumer -p 3333 -r consumer
+sleep 5s
+
+
+
+# Create accounts
+node app.js account -C -u admin -p 1111 -r companyAdmin -c AwesomeCorp
+sleep 5s
+node app.js account -C -u rep -p 2222 -r companyRep -c AwesomeCorp
+sleep 5s
