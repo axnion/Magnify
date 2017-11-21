ad461c9492417832bd8b6c03f141715404533d16
Francis <francis.menkes@gmail.com>
2017-11-21 09:13:11 -0300

    changed dependency versions to avoid eslint error

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
 import { shallow } from 'enzyme';
 import App from '../App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // eslint-disable-next-line function-paren-newline
   shallow(<App />);
 });
