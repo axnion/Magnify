0e0e9c1052115cf6a9b32be96fe3afd3e057e083
Leif Karlsson <leif.karlsson@stavos.ax>
2017-11-22 14:11:03 +0200

    backend: renames private key so it doesn't overwrite any existing keys

--- scripts/deploy-setup.sh
+++ scripts/deploy-setup.sh
@@ -9,11 +9,11 @@ PROD_PORT=2212
 PROD_USER="magnify"
 
 # Generate SSH keys used by git
-ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
+ssh-keygen -t rsa -f ~/.ssh/magnify_id_rsa -q -N ""
 
 # Copy private key to production server
 # Password: <secret>
-ssh-copy-id -p $PROD_PORT -o StrictHostKeyChecking=no $PROD_USER@$PROD_URI
+ssh-copy-id -i ~/.ssh/magnify_id_rsa -p $PROD_PORT -o StrictHostKeyChecking=no $PROD_USER@$PROD_URI
 
 # Add the production repo
 git remote add production ssh://magnify@magnify.today:2212/var/git/magnify
