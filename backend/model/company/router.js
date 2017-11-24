c100584b76fbdfab951a6d47f115d7881176ec91
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-24 13:31:18 +0100

    B: products route to company

--- backend/model/company/router.js
+++ backend/model/company/router.js
@@ -1,16 +1,17 @@
-const controller = require("./controller");
-const Router = require("express").Router;
+const controller = require('./controller');
+const Router = require('express').Router;
 const router = new Router();
 
 router
-  .route("/")
+  .route('/')
   .get((...args) => controller.find(...args))
   .post((...args) => controller.create(...args));
 
 router
-  .route("/:id")
+  .route('/:id')
   .put((...args) => controller.update(...args))
   .get((...args) => controller.findById(...args))
   .delete((...args) => controller.remove(...args));
+router.route('/products').get((...args) => controller.find(...args));
 
 module.exports = router;
