7999707f8231dff0c8835931de940675243dca49
wallstedt <fredrik.wallstedt@gmail.com>
2017-11-24 13:58:02 +0100

    B: small fixes to company/controller

--- backend/model/company/controller.js
+++ backend/model/company/controller.js
@@ -1,6 +1,23 @@
-const Controller = require("../../lib/controller");
-const companyFacade = require("./facade");
+const passport = require('passport');
+const Controller = require('../../lib/controller');
+const companyFacade = require('./facade');
+const config = require('../../config');
 
-class CompanyController extends Controller {}
+class CompanyController extends Controller {
+  createProducts(req, res, next) {
+    // Check authorization
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+      if (err) return res.status(500).json({ message: info });
+
+      if (!user || user.role !== config.userRole.companyAdmin) {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+
+      // TODO: implement fetch of products. Needs DB model for this
+      // →  Get { objs } where $company === $this.company
+      return res.status(200).json({ message: 'ok' });
+    })(req, res, next);
+  }
+}
 
 module.exports = new CompanyController(companyFacade);
