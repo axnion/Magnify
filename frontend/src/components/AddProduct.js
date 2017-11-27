91292beacd9c283eddce4019a26d4bdba6f2ca65
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-11-27 11:42:00 +0100

    Frontend: Added react-dropzone for a bit nicer file upload

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
 class AddProduct extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = { files: [] };
+  }
+
+  onDrop(files) {
+    this.setState({
+      files,
+    });
   }
 
   render() {
     return (
-      <div className="Index">
-        <h1>Add product</h1>
+      <div className="AddProduct">
+        <section>
+          <div className="dropzone">
+            <Dropzone accept=".pdf" onDrop={this.onDrop.bind(this)}>
+              <p>Drag and drop some files here, or click to select files to upload.</p>
+            </Dropzone>
+          </div>
+          <aside>
+            <h2>Dropped files</h2>
+            <ul>
+              {
+                this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              }
+            </ul>
+          </aside>
+        </section>
+        { this.state.files[0] ? (<button>Upload</button>) : (null) }
       </div>
     );
   }
