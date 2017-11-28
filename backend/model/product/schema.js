a434ac0f69bc58c202baa2143ff03b9d4e48e3a1
Axel Nilsson <an222yp@student.lnu.se>
2017-11-28 09:48:27 +0100

    backend: Added missing fields to product schema

--- backend/model/product/schema.js
+++ backend/model/product/schema.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const config = require('../../config');
 const Schema = mongoose.Schema;
-const Company = require('../company/schema');
-const Category = require('../category/schema');
 
-// TODO: Add material
 const productSchema = new Schema({
   name: { type: String, required: true },
   company: {
     type: mongoose.Schema.ObjectId,
     ref: 'Company',
     required: true
-  }
+  },
+  category: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Category'
+  },
+  material: [{
+    url: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    }
+  }]
 });
 
 let products;
