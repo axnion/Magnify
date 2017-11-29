e73878b1c92a4393752ffaa601389d9e1d048c48
Axel Nilsson <an222yp@student.lnu.se>
2017-11-29 17:22:31 +0100

    Removed unnessecary code from procuts

--- backend/model/product/facade.js
+++ backend/model/product/facade.js
@@ -2,9 +2,6 @@ const Facade = require('../../lib/facade');
 const productSchema = require('./schema');
 
 class ProductFacade extends Facade {
-  addProduct(company) {
-    return this.create(company)
-  }
 }
 
 module.exports = new ProductFacade(productSchema);
