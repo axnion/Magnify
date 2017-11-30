8085daef840e6b4dce5bd05def7da3d2da32a9da
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-11-30 12:33:34 +0100

    Frontend: Small change to path

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -24,7 +24,7 @@ const SideBar = ({ username, logout, role }) => (
       username === null ? undefined : <SideBarItem><Link to="/profile"><button className="sidebar-btn">Profile</button></Link></SideBarItem>
     }
     {
-      username === null ? undefined : <SideBarItem><Link to="/product"><button className="sidebar-btn">Add product</button></Link></SideBarItem>
+      username === null ? undefined : <SideBarItem><Link to="/addProduct"><button className="sidebar-btn">Add product</button></Link></SideBarItem>
     }
     {
       role === 'companyAdmin' ? <SideBarItem><Link to="/addRep"><button className="sidebar-btn">Add representative</button></Link></SideBarItem> : undefined
