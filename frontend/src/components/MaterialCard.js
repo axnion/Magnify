cae324e4fac9a32b1a66baf9f09a49eb68628f0e
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-12-03 15:48:00 +0100

    Frontend: Fixed Eslint and refactored to styled components

--- frontend/src/components/MaterialCard.js
+++ frontend/src/components/MaterialCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 // Material-ui
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import Download from 'material-ui/svg-icons/file/file-download';
 import TextField from 'material-ui/TextField';
 
@@ -10,48 +11,55 @@ import config from '../config';
 import StarRating from './StarRating';
 import ShowRating from './ShowRating';
 
-const cardStyle = {
-    padding: 25,
-};
 
-const downloadStyle = {
-    margin: 15,
-};
+const DownloadLink = styled('a')`
+    margin: 15px;
+`;
 
+const StyledCard = styled(Card)`
+    padding: 25px;
+    margin: 10px;
+`;
 
-const MaterialCardComponent = ({material, showRateStars, averageScore, numberOfRatings}) => (
-    <Card style={cardStyle}>
-        <CardHeader actAsExpander={true} showExpandableButton={true} title={material.title}>
-        </CardHeader>
-        <CardActions>
+const MaterialCardComponent = ({ material, showRateStars, averageScore, numberOfRatings }) => (
+  <StyledCard>
+    <CardHeader actAsExpander showExpandableButton title={material.title} />
+    <CardActions>
+      <div>
+        <DownloadLink download href={`${config.serverURI}${material.url}`} className="material-download">
+          <Download />
+        </DownloadLink>
         <div>
-            <a style ={downloadStyle} download href={`${config.serverURI}${material.url}`} className='material-download'>
-                <Download />
-            </a>  
-            <div>
-            {
-                numberOfRatings === 0 ? undefined : <ShowRating averageScore={averageScore} numberOfRatings={numberOfRatings}/>
+          {
+                numberOfRatings === 0 ? undefined : <ShowRating averageScore={averageScore} numberOfRatings={numberOfRatings} />
             }
-            {
-                !showRateStars ? undefined : StarRating({rating: 3, function(){}})
+          {
+                !showRateStars ? undefined : StarRating({ rating: 3, function() {} })
             }
-            </div>
         </div>
-        </CardActions>
-        <CardText expandable={true}>
-            <TextField 
-                hintText='Enter personal notes here'
-                floatingLabelText='Notes'
-            />
-        </CardText>
-    </Card>    
-)
+      </div>
+    </CardActions>
+    <CardText expandable>
+      <TextField
+        hintText="Enter personal notes here"
+        floatingLabelText="Notes"
+      />
+    </CardText>
+  </StyledCard>
+);
 
 MaterialCardComponent.propTypes = {
-    material: PropTypes.object, //eslint-disable-line
-    showRateStars: PropTypes.bool,
-    averageScore: PropTypes.number,
-    numberOfRatings: PropTypes.number
-  };
+  material: PropTypes.object, // eslint-disable-line
+  showRateStars: PropTypes.bool,
+  averageScore: PropTypes.number,
+  numberOfRatings: PropTypes.number,
+};
+
+MaterialCardComponent.defaultProps = {
+  material: null,
+  showRateStars: false,
+  averageScore: 0,
+  numberOfRatings: 0,
+};
 
 export default MaterialCardComponent;
