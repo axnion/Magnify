b680b44f1644d1f7b81e0724ec48c19a1de5bddc
leifkarlsson <lk222rc@student.lnu.se>
2017-12-04 20:58:04 +0200

    backend: removes unused methods in product facade

--- backend/model/product/facade.js
+++ backend/model/product/facade.js
@@ -1,17 +1,7 @@
 const Facade = require('../../lib/facade');
 const productSchema = require('./schema');
-const Grid = require('gridfs-stream');
-const mongoose = require('mongoose');
 
 class ProductFacade extends Facade {
-  saveMaterial(productId, material) {
-    return this.Schema.findOneAndUpdate(
-      { _id: productId },
-      { $push: { material: material } },
-      { new: true }
-    );
-  }
-
   findById(id) {
     return this.Schema.findOne({ _id: id })
       .populate('material');
@@ -20,17 +10,6 @@ class ProductFacade extends Facade {
   find() {
     return this.Schema.find()
       .populate('material');
-  }
-
-  getMaterialFile(materialId) {
-    const gfs = Grid(mongoose.connection.db, mongoose.mongo);
-    gfs.collection('material');
-
-    return new Promise((resolve, reject) => {
-      resolve(gfs.createReadStream({
-        _id: materialId
-      }));
-    });
   }
 }
 
