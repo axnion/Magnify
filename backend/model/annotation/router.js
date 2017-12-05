80e56a4dda2188fe6e0e76ca2f0bbc7c9be14063
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-12-05 16:33:25 +0100

    Merge fix

--- backend/model/annotation/router.js
+++ backend/model/annotation/router.js
@@ -3,11 +3,7 @@ const Router = require('express').Router;
 const router = new Router();
 
 router.route('/').post((...args) => controller.create(...args));
-<<<<<<< HEAD
-router.route('/:annotationId').get((...args) => controller.findById(...args));
-=======
 router.route('/:materialId').get((...args) => controller.findById(...args));
 router.route('/').get((...args) => controller.findAllAnnotationsByAccount(...args));
->>>>>>> 683603459806316d6023ca7407ef0e7fe0c3652c
 
 module.exports = router;
