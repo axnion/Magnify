9689a05c410ed4464375f680c9e27daeb1c15d31
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-12-05 14:21:46 +0100

    Frontend: Changed button to material ui

--- frontend/src/containers/ProductViewContainer.js
+++ frontend/src/containers/ProductViewContainer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Snackbar from 'material-ui/Snackbar';
 
+import RaisedButton from 'material-ui/RaisedButton';
 import { getAProduct } from '../actions/product';
 import { mockUploadAnnotation, mockGetAnnotation } from '../actions/annotation';
 import MaterialCard from '../components/MaterialCard';
@@ -22,17 +23,17 @@ class ProductView extends React.Component {
     dispatch(getAProduct(this.props.match.params.id));
   }
 
-  /*addAnnotationsToMaterial() {
+  /* addAnnotationsToMaterial() {
     const { dispatch } = this.props;
 
     const newMaterials = product
 
    return dispatch(mockGetAnnotation(materialId, this.props.auth.token));
-  }*/
+  } */
 
   saveAnnotation(annotation, materialId) {
     const { dispatch } = this.props;
-    
+
     if (annotation !== '') {
       dispatch(mockUploadAnnotation(annotation, materialId, this.props.auth.token))
         .then(() => {
@@ -49,28 +50,28 @@ class ProductView extends React.Component {
     const { auth, error, isWaiting } = this.props;
     let productHeadline = null;
     let materials = [];
-    
-    if (this.props.product) {
 
+    if (this.props.product) {
       materials = this.props.product.material;
       productHeadline =
             (<div>
               <h1>Product: {this.props.product.name}</h1>
               <h3>Company: {this.props.product.company.name}</h3>
               {
-                (auth.role === null || auth.role === 'consumer' || this.props.product.company._id !== auth.company) ? undefined : <Link to={`/material/${this.props.product._id}`}><button>Upload material</button></Link>
+                (auth.role === null || auth.role === 'consumer' || this.props.product.company._id !== auth.company) ? undefined : <Link to={`/material/${this.props.product._id}`}><RaisedButton primary label="Upload Material" /></Link>
               }
             </div>);
     } else { productHeadline = <h1>No product selected</h1>; }
     return (
       <div className="product">
         {productHeadline}
-        <h3>Materials (downloadable)</h3>
         {isWaiting && materials.length === 0 && <h2>Loading...</h2>}
         {!isWaiting && materials.length === 0 && <h2>Empty.</h2>}
         {!isWaiting && error && <h2>Error. {error} </h2>}
         {materials.length > 0 &&
-        <div style={{ opacity: isWaiting ? 0.5 : 1 }}>
+        <div style={{ opacity: isWaiting ? 0.5 : 1,
+          marginTop: '25px' }}
+        >
           {materials.map((material, key) =>
             <MaterialCard key={key} material={material} showRateStars={(auth.role === 'consumer')} averageScore={3.5} numberOfRatings={150} saveAnnotation={this.saveAnnotation} annotation={material.annotation} />)
         }
