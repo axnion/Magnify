34b5bbaadab2802b54ba675811bef8968be55fe2
wallstedt <fredrik.wallstedt@gmail.com>
2017-12-06 11:02:00 +0100

    Update Mongodb version to 3.4

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
-apt-get install -y mongodb-org
+apt-get install -y --allow-unauthenticated mongodb-org
 cp /etc/vagrant/mongodb.service /etc/systemd/system/mongodb.service
 sudo systemctl enable mongodb
 systemctl start mongodb
