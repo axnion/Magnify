d41e38b3d34ac7a3df77497308e0cf020883a926
Francis <francis.menkes@gmail.com>
2017-12-05 21:46:44 -0300

    B: corrected status code for product findById

--- backend/model/product/controller.js
+++ backend/model/product/controller.js
@@ -37,7 +37,7 @@ class ProductController extends Controller {
       // Return only the first document in the list
       return this.facade
       .findById(req.params.id, user)
-      .then(resp => res.status(201).json(resp[0]))
+      .then(resp => res.status(200).json(resp[0]))
       .catch(err => next(err));
     })(req, res, next);
   }
