8c63a0d99b8367e3369752ec54444458742f6d3d
Nicklas Björkendal <n.bjorkendal@gmail.com>
2017-12-09 11:59:17 +0100

    Added thread reducer

---
+++ frontend/src/reducers/thread.js
@@ -0,0 +1,66 @@
+import * as types from '../constants';
+
+export default (state = {
+  isWaiting: false,
+  threads: [],
+  error: null,
+}, action) => {
+  switch (action.type) {
+    case types.GET_THREADS:
+      return {
+        ...state,
+        isWaiting: true,
+      };
+    case types.GET_THREADS_SUCCESS:
+      return {
+        ...state,
+        threads: action.payload,
+        isWaiting: false,
+        error: null,
+      };
+    case types.GET_THREADS_ERROR:
+      return {
+        ...state,
+        isWaiting: false,
+        error: action.payload,
+      };
+    case types.GET_A_THREAD:
+      return {
+        ...state,
+        isWaiting: true,
+      };
+    case types.GET_A_THREAD_SUCCESS:
+      return {
+        ...state,
+        threads: [...state.threads, action.payload],
+        isWaiting: false,
+        error: null,
+      };
+    case types.GET_A_THREAD_ERROR:
+      return {
+        ...state,
+        isWaiting: false,
+        error: action.payload,
+      };
+    case types.CREATE_THREAD:
+      return {
+        ...state,
+        isWaiting: true,
+      };
+    case types.CREATE_THREAD_SUCCESS:
+      return {
+        ...state,
+        isWaiting: false,
+        threads: [...state.threads, action.payload],
+        error: null,
+      };
+    case types.CREATE_THREAD_ERROR:
+      return {
+        ...state,
+        isWaiting: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
