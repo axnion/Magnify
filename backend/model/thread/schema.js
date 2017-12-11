3a8d93245ee5e23b9b7faaa805e99105365632f3
leifkarlsson <lk222rc@student.lnu.se>
2017-12-11 14:34:01 +0200

    B: adds thread db schema

---
+++ backend/model/thread/schema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp');
+const Schema = mongoose.Schema;
+
+const threadSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  posts: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Post'
+  }],
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Account',
+    required: true
+  }
+  // tags: [{ type: mongoose.Schema.ObjectId, ref: 'Tag' }]
+});
+
+// Adds createdAt and updatedAt automatically
+threadSchema.plugin(timestamps);
+
+let thread;
+
+// Used for testing to make sure model is not already in database
+try {
+  thread = mongoose.model('thread');
+} catch (error) {
+  thread = mongoose.model('thread', threadSchema);
+}
+
+module.exports = thread;
