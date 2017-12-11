35de295152970e01fdc9a472c8c7e083f2a30e7b
Axel Nilsson <an222yp@student.lnu.se>
2017-12-11 15:35:13 +0100

    B: Basic create thread endpoint is implemented

--- backend/model/thread/controller.js
+++ backend/model/thread/controller.js
@@ -1,6 +1,29 @@
+const passport = require('passport');
 const Controller = require('../../lib/controller');
 const threadFacade = require('./facade');
 
-class ThreadController extends Controller {}
+class ThreadController extends Controller {
+  createThread(req, res, next) {
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+      if (err) return res.status(500).json({ message: info });
+
+      if (!user) {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+
+      const thread = req.body;
+      thread.author = user.id;
+
+      return threadFacade.create(thread)
+      .then((result) => {
+        res.status(201).json(result);
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+    })(req, res, next);
+  }
+}
 
 module.exports = new ThreadController(threadFacade);
