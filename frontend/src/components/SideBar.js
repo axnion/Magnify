dd94bcafbe40cfaa5be235e2875ac4ea71e432a7
wallstedt <fredrik.wallstedt@gmail.com>
2017-12-20 16:28:29 +0100

    F: better CSS for aligning when screen is narrow

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import {
-  Link
+  Link,
 } from 'react-router-dom';
 import Badge from 'material-ui/Badge';
 
@@ -42,69 +42,93 @@ const SideBarDivider_old = styled('div')
 
 const SideBar = ({
     username,
-    logout,
+    logout, 
     role,
     unseenThreads,
-    updateUnseen
-  }) => ( <
-    SideBarDivider className = "sidebar" > {
-      username === null ? undefined : < SideBarItem > < Link to = "/profile"
+    updateUnseen,
+  }) => ( < SideBarDivider className = "sidebar" > {
+      username === null ? undefined : < SideBarItem > < Link
+      to = "/profile"
       onClick = {
         updateUnseen
-      } > < button className = "sidebar-btn" > Profile < /button></Link > < /SideBarItem>
+      } >
+      <
+      button className = "sidebar-btn" > Profile < /button>  <
+      /Link >   <
+      /SideBarItem>
     } {
-      username === null ? undefined : < SideBarItem > < Link to = "/products"
+      username === null ? undefined : < SideBarItem > < Link
+      to = "/products"
       onClick = {
-        updateUnseen
-      } > < button className = "sidebar-btn" > Products < /button></Link > < /SideBarItem>
+          updateUnseen
+        } >
+        <
+        button className = "sidebar-btn" > Products < /button>  <
+        /Link >   <
+        /SideBarItem>
     } {
-      (username === null || role === 'consumer') ? undefined: < SideBarItem > < Link to = "/addProduct"
+      (username === null || role === 'consumer') ? undefined: < SideBarItem > < Link
+      to = "/addProduct"
       onClick = {
-        updateUnseen
-      } > < button className = "sidebar-btn" > Add product < /button></Link > < /SideBarItem>
+          updateUnseen
+        } >
+        <
+        button className = "sidebar-btn" > Add product < /button>  <
+        /Link >   <
+        /SideBarItem>
     } {
-      role === 'companyAdmin' ? < SideBarItem > < Link to = "/addRep"
+      role === 'companyAdmin' ? < SideBarItem > < Link
+      to = "/addRep"
       onClick = {
-        updateUnseen
-      } > < button className = "sidebar-btn" > Add representative < /button></Link > < /SideBarItem> : undefined
-    } <
-    SideBarItem >
+          updateUnseen
+        } >
+        <
+        button className = "sidebar-btn" > Add representative < /button>  <
+        /Link >   <
+        /SideBarItem> : undefined
+    } < SideBarItem >
     <
     Link to = "/forum"
     onClick = {
       updateUnseen
-    } > < button className = "sidebar-btn" > Forum < /button></Link > {
-      role === 'companyRep' ? < Badge badgeContent = {
-        unseenThreads.length
+    } >
+    <
+    button className = "sidebar-btn" > Forum < /button>  <
+    /Link > {
+    role === 'companyRep' ? < Badge badgeContent = {
+      unseenThreads.length
+    }
+    secondary style = {
+      {
+        padding: '0px 24px 18px 12px'
       }
-      secondary style = {
-        {
-          padding: '0px 24px 18px 12px'
-        }
-      }
-      /> : undefined } < /
-      SideBarItem > {
-        username === null ? < SideBarItem > < Link to = "/login" > < button className = "sidebar-btn" > Log in < /button></Link > < /SideBarItem> : <
-        SideBarItem > < button className = "sidebar-btn"
-        onClick = {
-          () => logout()
-        } > Log out < /button></SideBarItem >
-      } {
-        username === null ? < SideBarItem > < Link to = "/register" > < button className = "sidebar-btn" > Create an account < /button></Link > < /SideBarItem> : undefined
-      } <
-      /SideBarDivider>
-    );
+    }
+    /> : undefined }   <
+    /SideBarItem > {
+    username === null ? < SideBarItem > < Link to = "/login" > < button className = "sidebar-btn" > Log in < /button></Link > < /SideBarItem> : <SideBarItem > <button
+    className = "sidebar-btn"
+    onClick = {
+      () => logout()
+    } >
+    Log out <
+    /button>  <
+    /SideBarItem >
+  } {
+    username === null ? < SideBarItem > < Link to = "/register" > < button className = "sidebar-btn" > Create an account < /button></Link > < /SideBarItem> : undefined
+  } <
+  /SideBarDivider>
+);
 
-    SideBar.propTypes = {
-      username: PropTypes.string,
-      logout: PropTypes.func.isRequired,
-      role: PropTypes.string,
-      unseenThreads: PropTypes.arrayOf(PropTypes.any).isRequired,
-    };
+SideBar.propTypes = {
+  username: PropTypes.string,
+  logout: PropTypes.func.isRequired,
+  role: PropTypes.string,
+  unseenThreads: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
 
-    SideBar.defaultProps = {
-      username: null,
-      role: null
-    };
+SideBar.defaultProps = {
+  username: null,
+  role: null,
+};
 
-    export default SideBar;
+export default SideBar;
